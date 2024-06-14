98634c750a112b2757221316d098a2f5dbd46557
Jack Rellinger <jackrellinger@Jacks-MacBook-Pro-3.local>
2024-06-14 12:03:11 -0400

    Chapter 15 complete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
  
 const FormSchema = z.object({
   id: z.string(),
@@ -19,6 +22,10 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export type State = {
     errors?: {
       customerId?: string[];
@@ -27,43 +34,45 @@ export type State = {
     };
     message?: string | null;
 };
- 
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-
 
 export async function createInvoice(prevState: State, formData: FormData) {
+    // Validate form using Zod
     const validatedFields = CreateInvoice.safeParse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
     });
-
-    // If form validation fails, return errors early, Otherwise, continue
+   
+    // If form validation fails, return errors early. Otherwise, continue.
     if (!validatedFields.success) {
-        return {
-          errors: validatedFields.error.flatten().fieldErrors,
-          message: 'Missing Fields. Failed to Create Invoice.',
-        };
-      }
-
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
+    }
+   
+    // Prepare data for insertion into the database
     const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
+   
+    // Insert data into the database
     try {
-        await sql`
-            INSERT INTO invoices (customer_id, amount, status, date)
-            VALUES (${customerID}, ${amountInCents}, ${status}, ${date})
-            `;
+      await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
     } catch (error) {
-        return {
-            message: 'Database Error: Failed to Create Invoice.',
-        };
+      // If a database error occurs, return a more specific error.
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      };
     }
-    
+   
+    // Revalidate the cache for the invoices page and redirect the user.
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-}
+  }
 
 export async function updateInvoice(
     id: string,
@@ -100,7 +109,7 @@ export async function updateInvoice(
     redirect('/dashboard/invoices');
   }
 
-  
+
   export async function deleteInvoice(id: string) {
     // throw new Error('Failed to Delete Invoice');
     
@@ -110,5 +119,24 @@ export async function updateInvoice(
         return { message: 'Deleted Invoice.' };
     } catch (error) {
         return { message: 'Database Error: Failed to Delete Invoice' };
+    }
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
     }
   }
